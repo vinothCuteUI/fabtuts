07818c33be9ded38aaad09826e39df9167b70a59
Vinoth UI <placeofvino@gmail.com>
2022-10-04 14:03:04 +0530

    update

---
+++ js/css.js
@@ -0,0 +1,19 @@
+let cssData = [
+	{
+		padding:[
+			{
+				padding:[
+					top: 0, 
+					right: 0, 
+					bottom: 0, 
+					left: 0
+				],
+				paddingTop:0,
+				paddingRight:0,
+				paddingBottom:0,
+				paddingLeft:0,
+
+			}
+		]
+	}
+]
--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-let onLiveCode = function(){
-	let _htmlCode = document.getElementById("htmlCodeEditor").value;
-	let _cssCode = "<style>"+document.getElementById("cssCodeEditor").value+"</style>";
-	// let _jsCode = "<scri"+"pt>"+document.getElementById("jsCodeEditor").value+"</scri"+"pt>";
-	let _showOutput = document.getElementById("codeOutPut").contentWindow.document;
-	_showOutput.open();
-	_showOutput.write(_cssCode+_htmlCode);
-	_showOutput.close();
-} 
+// let onLiveCode = function(){
+// 	let _htmlCode = document.getElementById("htmlCodeEditor").value;
+// 	let _cssCode = "<style>"+document.getElementById("cssCodeEditor").value+"</style>";
+// 	// let _jsCode = "<scri"+"pt>"+document.getElementById("jsCodeEditor").value+"</scri"+"pt>";
+// 	let _showOutput = document.getElementById("codeOutPut").contentWindow.document;
+// 	_showOutput.open();
+// 	_showOutput.write(_cssCode+_htmlCode);
+// 	_showOutput.close();
+// } 
+
+cssData.forEach(function(obj, indx) {
+	
+});
