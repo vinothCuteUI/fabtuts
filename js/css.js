66220e893736cff2a716c246bdfe1501a0ba1165
vinothCuteUI <placeofvino@gmail.com>
2022-11-29 18:38:39 +0530

    updated

--- js/css.js
+++ js/css.js
@@ -1,6 +1,6 @@
 let cssData = [
 	{
-		padding:[
+		"padding":[
 			{
 				"padding": "20px 20px 20px 20px",
 				"padding-top": "10px",
@@ -8,12 +8,18 @@ let cssData = [
 				"padding-bottom":"10px",
 				"padding-left":"10px",
 			},
+			
 			{
-				"shorthand":["top", "right", "bottom", "left"]
+				"htmlView":`
+							<p class='code-mirror'>
+								<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>div</span><span class='fb-tag fb-bracket'>></span>
+								<span class='fb-cn'>Padding</span>
+								<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>/div</span><span class='fb-tag fb-bracket'>></span>
+							</p>`
 			}
 			
 		],
-		border:[
+		"border":[
 			{
 				"border": "5px solid red",
 				"border-width": "2px",
@@ -24,12 +30,18 @@ let cssData = [
 				"border-bottom-style": "dotted",
 				"border-left-style": "solid",
 			},
+			
 			{
-				"shorthand":["width", "style", "color"]
+				"htmlView":`
+				<p class='code-mirror'>
+					<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>div</span><span class='fb-tag fb-bracket'>></span>
+					<span class='fb-cn'>Border</span>
+					<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>/div</span><span class='fb-tag fb-bracket'>></span>
+				</p>`
 			}
 			
 		],
-		margin:[
+		"margin":[
 			{
 				"margin": "20px 20px 20px 20px",
 				"margin-top": "10px",
@@ -37,10 +49,98 @@ let cssData = [
 				"margin-bottom":"10px",
 				"margin-left":"10px",
 			},
+			
 			{
-				"shorthand":["top", "right", "bottom", "left"]
+				"htmlView":`
+				<p class='code-mirror'>
+					<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>div</span><span class='fb-tag fb-bracket'>></span>
+					<span class='fb-cn'>Margin</span>
+					<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>/div</span><span class='fb-tag fb-bracket'>></span>
+				</p>`
 			}
 			
+		],
+		"list-style":[
+			{
+				"list-style": `square inside url("sqpurple.gif")`,
+				"list-style-type": `circle`,
+				"list-style-position":`outside`,
+				"list-style-image":`url("sqpurple.gif")`
+				
+			},
+			
+			{
+				"htmlView":`
+				<p class='code-mirror'>
+					<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>ul</span><span class='fb-tag fb-bracket'>></span>
+						<br/>
+						<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>li</span><span class='fb-tag fb-bracket'>></span>Apple<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>/li</span><span class='fb-tag fb-bracket'>></span><br/>
+						<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>li</span><span class='fb-tag fb-bracket'>></span>Orange<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>/li</span><span class='fb-tag fb-bracket'>></span><br/>
+						<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>li</span><span class='fb-tag fb-bracket'>></span>Lemon<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>/li</span><span class='fb-tag fb-bracket'>></span><br/>
+					<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>/ul</span><span class='fb-tag fb-bracket'>></span>
+				</p>`
+			}
+			
+		],
+		"background":[
+			{
+				"background-color":"lightgray",
+				"background-image":"url('https://www.treehugger.com/thmb/Y5reVpe2tn68Wqn4YQrO_e5vJQ0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2018__06__nature-phpoto-day-squirrel-6b50719b524c4ecbb3152a32baa38367.jpg')",
+				"background-position":"left top",
+				"background-position-x":"left",
+				"background-position-y":"bottom",
+				"background-size":"cover",
+				"background-repeat":"no-repeat",				
+				"background-origin":"padding-box",
+				"background-clip":"border-box",
+				"background-attachment":"fixed",
+				"background-blend-mode":"lighten"
+			},
+			{
+				"htmlView":`
+				<p class='code-mirror'>
+					<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>div</span><span class='fb-tag fb-bracket'>></span>
+					<span class='fb-cn'>background</span>
+					<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>/div</span><span class='fb-tag fb-bracket'>></span>
+				</p>`
+			}
+		],
+		"text-alignment":[
+			{
+				"color":"red",
+				"text-align":"center",
+				"text-align-last":"justify",
+			    "direction":"rtl",
+				"unicode-bidi":"bidi-override",
+				"vertical-align":"baseline"
+
+			},
+			{
+				"htmlView":`
+				<p class='code-mirror'>
+					<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>p</span><span class='fb-tag fb-bracket'>></span>
+					<span class='fb-cn'>Sed sit amet nunc sit amet nulla auctor congue a ac odio. Suspendisse potenti. Aenean sollicitudin nisl tincidunt, iaculis urna sed, dignissim dui. Sed id hendrerit neque. Mauris at sodales velit. Pellentesque porta et justo vel vehicula. Vivamus viverra libero mollis lobortis tempor. Nullam pulvinar at justo quis imperdiet. Aliquam sed aliquet tortor. Sed convallis ex posuere ligula ullamcorper, sit amet malesuada lacus lacinia. Proin viverra viverra sapien, a interdum lacus bibendum eget.</span>
+					<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>/p</span><span class='fb-tag fb-bracket'>></span>
+				</p>`
+			}
+		],
+		"text-decoration":[
+			{
+				"text-decoration-line":"overline",
+				"text-decoration-color":"red",
+				"text-decoration-style":"dotted",
+				"text-decoration-thickness":"5px",
+				"text-decoration":"underline red dashed 5px"
+			},
+			{
+				"htmlView":`
+				<p class='code-mirror'>
+					<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>h3</span><span class='fb-tag fb-bracket'>></span>
+					<span class='fb-cn'>Css Text Decoration</span>
+					<span class='fb-tag fb-bracket'><</span><span class='fb-tag'>/h3</span><span class='fb-tag fb-bracket'>></span>
+				</p>`
+			}
 		]
+
 	}
 ]
--- js/main.js
+++ js/main.js
@@ -8,11 +8,17 @@
 // 	_showOutput.close();
 // } 
 
-let _props = "padding", _cssList, _divElm, _optContent, _cssProperties, 
-_propertyOption, _outPutTag, _htmlView, _outPtElm, _cssProp, isClick, 
+let _props = "padding", _demoElmId, _cssList, _divElm, _optContent, _cssProperties, 
+_propertyOption, _outPutTag, _htmlView, _outPtElm, _cssProp, _isClick, 
 _dropDownCss, _cssOptions, _cssPropList, getActiveOpt, _newCssKeyVal, 
 _cssKey, _cssVal, _cssValInpt, _getPropsVal, _inptBox, _addShorthand, 
 _getProps, _getId, _editElm;
+
+let _getEditCssObj = {}, _updateVal = [], 
+ _getUptStyle, updateStyle,
+_headTag = document.head || document.getElementsByTagName('head')[0],
+_style = document.createElement('style');
+_headTag.appendChild(_style);
 
 //load selected css object array 
 let newArrObj = [];
@@ -34,17 +40,12 @@ var _sideNav = document.getElementsByClassName("side-nav")[0];
 for(let i in cssData[0]){
 	var _navli = document.createElement("li");
 	var _navA = document.createElement("a");
-
 	_navA.setAttribute("class", "csslist");
 	_navA.setAttribute("href", "#"+i);
 	_navA.innerText = i;
-	
-
 	_navli.appendChild(_navA);
 	_sideNav.appendChild(_navli);
-	document.getElementsByClassName("csslist")[0].classList.add("nav-active");
-
-
+	document.getElementsByClassName("csslist")[0].classList.add("active");
 }
 
 //Select css nav to edit
@@ -52,119 +53,101 @@ for(let i = 0; i < _cssList.length; i++){
 
 	_cssList[i].addEventListener("click", (e)=>{
 		e.preventDefault();
-
 		var _node = Array.prototype.slice.call(_cssList);
         var _indx = _node.indexOf(e.target);
-        document.getElementsByClassName("nav-active")[0].classList.remove("nav-active");
-        document.getElementsByClassName("csslist")[_indx].classList.add("nav-active");
+        
+        document.querySelectorAll(".side-nav li a.active")[0].classList.remove("active");
+        document.querySelectorAll(".side-nav li a.csslist")[_indx].classList.add("active");
         
 		_props = e.target.href.split("#")[1];
-
-		 if(isClick != _indx){
-		 	cssData.forEach(function(obj, indx) {
-				newArrObj.shift();
-				newArrObj.push(obj[_props]);
-				toCss(_props, {[_props]:obj[_props][0][_props]});
-			});
-
+		 if(_isClick != _indx){
+		 	onCssLoad();
 		 }
-		 isClick = _indx;
+		 _isClick = _indx;
 	})
 }
 
 
 
-
-
-//On side nave click function
+//On css option nave enable function
 for(let i = 0; i < _dropDownCss.length; i++){
 	_dropDownCss[i].addEventListener("click", function(e){
 		e.preventDefault();
 		let _node = this.parentNode;
 		
 		let dropNave = _node.getElementsByClassName("css-dropdown-nav");
-		dropNave[0].classList.toggle("css-active-nav");
+		dropNave[0].classList.toggle("active");
 	})
 }
+let _initObjProp, _initObjVal;
 
 
-//Main css and demo function
-let toCss = function(elm, cssPr){
-	
-	document.getElementsByClassName("active-demo")[0].classList.remove("active-demo");
-    document.getElementById(elm+"-demo").classList.add("active-demo");
-
+//Initial load 
+let onCssLoad = ()=>{
+	let cssPr;
+	_getEditCssObj = {};
+	cssData.forEach(function(obj, indx) {
+		newArrObj.shift();
+		newArrObj.push(obj[_props]);
+		
+	});
+	document.querySelectorAll(".demo-tags.active")[0].classList.remove("active");
+    document.getElementById(_props+"-demo").classList.add("active");
+    _demoElmId = document.getElementById(_props+"-demo");
 	//clear creates child elements
 	_propertyOption.innerHTML = "";
-	// _outPutTag.innerHTML = "";
 	_cssProperties.innerHTML = "";
 	_htmlView.innerHTML = "";
 
-	
 	//load dynamic content
-	_cssProp.innerText = elm;
-	_htmlView.innerText = elm;
-	
+	_cssProp.innerText = _props;
 
-	//create dynamic elements & css
-	_outPtElm = document.getElementById(elm+"-demo"); 
-	_outPtElm.innerText = "Demo of " + elm;
-	
-	//load shorthand css
-	newArrObj.forEach( function(obj, indx) {
-		for(let s in obj[1]){
-			_addShorthand = obj[1][s];
-		}
-	});
-	
-	//pre load css
-	for(let c in cssPr){
-		_newCssKeyVal = document.createElement("div");
-		_cssKey = document.createElement("div");
-		_cssVal = document.createElement("div");
-		
-		
-		_newCssKeyVal.setAttribute("class", "props-list");
-		_newCssKeyVal.setAttribute("data-props", c);
-		
-		_cssKey.setAttribute("class", "csskey");
-		_cssVal.setAttribute("class", "cssval");
-
-		_cssKey.innerText = c+" :";
-
-		_getPropsVal = cssPr[c].split(" ");
-		
-	
-		for(let i = 0; i < _getPropsVal.length; i++){
-			_cssValInpt = document.createElement("input");
-			_cssValInpt.setAttribute("type", "text");
-			_cssValInpt.setAttribute("class", "css-inpt"+" "+ c+"-"+_addShorthand[i]);
-			// _cssValInpt.setAttribute("class", c+"-"+_addShorthand[i]);
-			
-			_cssValInpt.setAttribute("data-type", c+"-"+_addShorthand[i]);
-			
-			
-			_cssValInpt.value = _getPropsVal[i];
-			_cssVal.appendChild(_cssValInpt);
-
-		}
-		
-		// _cssVal.innerText = cssPr[c]+";";
-		_newCssKeyVal.appendChild(_cssKey);
-		_newCssKeyVal.appendChild(_cssVal);
-		_cssProperties.appendChild(_newCssKeyVal);
-		_outPtElm.style[c] = cssPr[c];
-
-		_inptBox = document.getElementsByClassName("css-inpt");
-		for(let n = 0; n < _inptBox.length; n++){
-			_inptBox[n].addEventListener("keyup", onEdit);
-		}
-
-	}
 
 	//load css properties
 	newArrObj.forEach( function(obj, indx) {
+		
+		_htmlView.innerHTML = obj[1]["htmlView"];
+		_initObjProp = Object.keys(obj[0])[0];
+		_initObjVal = Object.values(obj[0])[0];
+		
+		// console.log(getObj)
+		cssPr = {[Object.keys(obj[0])[0]]:Object.values(obj[0])[0]};
+		_getEditCssObj[_initObjProp] = _initObjVal;
 
+		for(let c in cssPr){
+			_newCssKeyVal = document.createElement("div");
+			_cssKey = document.createElement("div");
+			_cssVal = document.createElement("div");
+			_newCssKeyVal.setAttribute("class", "props-list");
+			_newCssKeyVal.setAttribute("data-props", c);
+			_cssKey.setAttribute("class", "csskey");
+			_cssVal.setAttribute("class", "cssval");
+			_cssKey.innerText = c+" :";
+
+			//Create inputs 
+			_getPropsVal = cssPr[c].split(" ");
+			for(let i = 0; i < _getPropsVal.length; i++){
+				_cssValInpt = document.createElement("input");
+				_cssValInpt.setAttribute("type", "text");
+				_cssValInpt.setAttribute("class", "css-inpt"+" "+ c);
+				_cssValInpt.setAttribute("data-type", c);
+				_cssValInpt.value = _getPropsVal[i];
+				_cssVal.appendChild(_cssValInpt);
+
+			}
+			
+			// _cssVal.innerText = cssPr[c]+";";
+			_newCssKeyVal.appendChild(_cssKey);
+			_newCssKeyVal.appendChild(_cssVal);
+			_cssProperties.appendChild(_newCssKeyVal);
+
+
+			_inptBox = document.getElementsByClassName("css-inpt");
+			for(let n = 0; n < _inptBox.length; n++){
+				_inptBox[n].addEventListener("keyup", onEdit);
+			}
+
+		}
 		for(let i in obj[0]){
 			
 			_cssPropList = document.createElement("li");
@@ -174,35 +157,26 @@ let toCss = function(elm, cssPr){
 			_cssPropList.innerText = i;
 			_propertyOption.appendChild(_cssPropList);
 			_getActiveOpt = document.getElementsByClassName("css-options")[0];
-			_getActiveOpt.classList.add("active-option");
+			_getActiveOpt.classList.add("active");
 			_getActiveOpt.setAttribute("data-checked", true);
 
 		}
 		_cssOptions = document.getElementsByClassName("css-options");
+		
 		for(let i = 0; i < _cssOptions.length; i++){
 			_cssOptions[i].addEventListener("click", onCssOption, false);
 
 		}
 	});
 
-
-}
-
-
-//Initial load 
-let onCssLoad = (csp)=>{
-	cssData.forEach(function(obj, indx) {
-		_outPtElm = document.getElementById("padding-demo"); 
-		newArrObj.shift();
-		newArrObj.push(obj[_props]);
-		toCss(_props, { [_props]:obj[_props][0][_props]});
-		
-	});
+	//update css
+	onUpdateCss();
 
 }
 
 //Select css option properties
 let onCssOption = function(e){
+
 	let _getCss = e.target.dataset.id;
 	if(e.target.dataset.checked == "true"){
 		e.target.dataset.checked = false;
@@ -210,13 +184,17 @@ let onCssOption = function(e){
 		e.target.dataset.checked = true;
 	}
 	let _isChecked = e.target.dataset.checked;
-	e.target.classList.toggle("active-option");
+	e.target.classList.toggle("active");
 	
 	if(_isChecked == "true"){
 		newArrObj.forEach(function(cssOp, indVal) {
 			for(let i in cssOp[0]){
 				
 				if(_getCss == i){
+
+					//Add css props obj
+					_getEditCssObj[i] = cssOp[0][i];
+					
 					_newCssKeyVal = document.createElement("div");
 					_cssKey = document.createElement("div");
 					_cssVal = document.createElement("div");
@@ -231,49 +209,27 @@ let onCssOption = function(e){
 					// _cssVal.innerText = cssOp[i]+";";
 
 					 _getPropsVal = cssOp[0][i].split(" ");
-					if(_props == _getCss){
-						for(let x = 0; x < _getPropsVal.length; x++){
-							_cssValInpt = document.createElement("input");
-							_cssValInpt.setAttribute("type", "text");
-							_cssValInpt.setAttribute("class", "css-inpt"+" "+ i+"-"+_addShorthand[x]);
-							
-							
-							_cssValInpt.setAttribute("data-type", i+"-"+_addShorthand[x]);
-							
-							
-							_cssValInpt.value = _getPropsVal[x];
-							_cssVal.appendChild(_cssValInpt);
+					
+					for(let x = 0; x < _getPropsVal.length; x++){
+					
+						_cssValInpt = document.createElement("input");
+						_cssValInpt.setAttribute("type", "text");
+						_cssValInpt.setAttribute("class", "css-inpt"+" "+ i);
+						// _cssValInpt.setAttribute("class", i);
 
+						_cssValInpt.setAttribute("data-type", i);
 
-						}
-					}else{
-						for(let x = 0; x < _getPropsVal.length; x++){
+						_cssValInpt.value = _getPropsVal[x];
+						_cssVal.appendChild(_cssValInpt);
 						
-							_cssValInpt = document.createElement("input");
-							_cssValInpt.setAttribute("type", "text");
-							_cssValInpt.setAttribute("class", "css-inpt", i);
-							// _cssValInpt.setAttribute("class", i);
-
-							_cssValInpt.setAttribute("data-type", i);
-
-							_cssValInpt.value = _getPropsVal[x];
-							_cssVal.appendChild(_cssValInpt);
-							
-							let _findCssVal = document.getElementsByClassName(i);
-							if(_findCssVal.length > 0){
-								document.getElementsByClassName(i)[0].setAttribute("disabled", true);
-							}
-
-						}
 					}
-
-
+					
 					_newCssKeyVal.appendChild(_cssKey);
 					_newCssKeyVal.appendChild(_cssVal);
 
 					_cssProperties.appendChild(_newCssKeyVal);
-					_outPtElm.style[i] = cssOp[0][i];
-					
+					//update css
+					onUpdateCss();
 					_inptBox = document.getElementsByClassName("css-inpt");
 
 					for(let n = 0; n < _inptBox.length; n++){
@@ -287,72 +243,56 @@ let onCssOption = function(e){
 			}
 		});
 	}else{
+
+		//Delete css props obj
+		delete _getEditCssObj[_getCss];
+
+		//Remove disabled css props
 		let _cssProsElem = document.getElementsByClassName("props-list");
-		let _cssInpt = document.getElementsByClassName("css-inpt");
 		for(let i = 0; i < _cssProsElem.length; i++){
 			if(_cssProsElem[i].dataset.props == e.target.dataset.id){
-				
 				//Remove added css props
 				_cssProsElem[i].remove();
-				removeEdit(e.target.dataset.id);
-
-				//updated css
-				for(let j = 0; j < _cssInpt.length; j++){
-					
-					if(_cssInpt[j].dataset.type == e.target.dataset.id){
-						_getProps = _cssInpt[j].dataset.type;
-						 _getId = _getProps.split("-")[0];
-						 _editElm = document.getElementById(_getId+"-demo");
-						_editElm.style[_getProps] = _cssInpt[j].value;
-					}
-				}
-				//remove disable
-				let _rmCssVal = document.getElementsByClassName(e.target.dataset.id);
-				if(_rmCssVal.length != 0){
-					document.getElementsByClassName(e.target.dataset.id)[0].removeAttribute("disabled");
-				}
 				
-
 			}
 		}
-		
-		
-		
-		
+
+		//update css
+		onUpdateCss();
 	}
 }
 
 
-
 //Edit css props function
 let onEdit = function(str){
-	console.log(str)
+	_updateVal = [];
 	 _getProps = str.target.dataset.type;
-	 _getId = _getProps.split("-")[0];
-	 _editElm = document.getElementById(_getId+"-demo");
-	_editElm.style[_getProps] = str.target.value;
-}
+	 _getEditVal = document.getElementsByClassName(_getProps);
+	 for(let i = 0; i < _getEditVal.length; i++){
+	 	
+	 	_updateVal.push(_getEditVal[i].value);
+	 }
+	 _getEditCssObj[_getProps] = _updateVal.join(" ");
 
-//remove css props function
-let removeEdit = function(str){
-	 _getId =  str.split("-")[0];
-	 _editElm = document.getElementById(_getId+"-demo");
-	_editElm.style.removeProperty(str);
-	// element.style.removeProperty("width");
-
+	 //Update css
+	 onUpdateCss();
 }
 
 
-// function clearChild(str) {
-	
-//     var e = document.querySelector(str);
-    
-//     // //e.firstElementChild can be used.
-//     // var child = e.lastElementChild; 
-//     // while (child) {
-//     //     e.removeChild(child);
-//     //     child = e.lastElementChild;
-//     // }
-// }
 
+
+// update edit css poperties
+let onUpdateCss = ()=>{
+	_getUptStyle = "";
+	updateStyle = "";
+	_style.innerHTML = "";
+	for(let stl in _getEditCssObj){
+	 	_getUptStyle += stl+":"+_getEditCssObj[stl]+";";
+	 }
+	 updateStyle = "#"+_props+"-demo"+"{"+_getUptStyle+"}";
+	_style.appendChild(document.createTextNode(updateStyle));
+}
+
+
+//Initial load css 
 window.load = onCssLoad();
